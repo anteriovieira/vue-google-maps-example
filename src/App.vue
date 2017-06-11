2938e3b0e46057c9c1871fb329ec7830b9b58874
Antério vieira <anteriovieira@gmail.com>
2017-06-10 23:45:04 -0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+  #app {
+    display: flex;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+  }
+</style>
---
+++ src/components/Map.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="map-container">
+    <gmap-map :center="{lat:1.38, lng:103.8}" :zoom="12">
+      <gmap-marker :position="{lat:1.38, lng:103.8}">
+      </gmap-marker>
+      <gmap-info-window :position="{lat:1.38, lng:103.8}">
+        Hello world!
+      </gmap-info-window>
+    </gmap-map>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'map'
+  }
+</script>
+
+<style>
+  .map-container, .vue-map-container {
+    display: flex;
+    width: 100%;
+  }
+</style>
